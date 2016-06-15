93c0b6b4aeded9b8d6032da976e6107f97a64042
Tres Trantham <tres@trestrantham.com>
2016-06-14 17:06:31 -0700

    And error response and data parsing

--- lib/mopidy.ex
+++ lib/mopidy.ex
@@ -84,29 +84,111 @@ defmodule Mopidy do
   Returns dict
   """
   def api_request(data \\ %{}) do
-    Mopidy.post nil,
-    [
-      body: Map.merge(
-        %{
-          id: "1",
-          jsonrpc: "2.0"
-        },
-        data
-      )
-    ]
+    body = Map.merge(%{id: "1", jsonrpc: "2.0"}, data)
+    response = Mopidy.post nil, [body: body]
 
-    # case request(method, endpoint, body) do
-    #   {:ok, response} ->
-    #     (case response.body do
-    #       %{"errors" => errors} ->
-    #         error = List.first(errors)
-    #         {:error, %{error: error["errorType"], message: error["message"]}}
-    #       _ ->
-    #         {:ok, response.body}
-    #     end)
-    #   {:error, reason} ->
-    #     {:error, %{error: "bad_request", message: reason}}
-    # end
+    with %HTTPotion.Response{body: body} <- response do
+      {:ok, body}
+    else
+      %HTTPotion.ErrorResponse{message: message} -> {:error, message}
+      {:error, error} -> {:error, error}
+      _ -> {:error, "could not process request"}
+    end
+  end
+
+  def api_request(data, data_type) do
+    case Mopidy.api_request(data) do
+      {:ok, %{"error" => error} = body} ->
+        error_response(body)
+      {:ok, body} ->
+        case data_type do
+          %Mopidy.TlTrack{} ->
+            {:ok, parse_tl_tracks(body["result"], [])}
+          %Mopidy.Track{} ->
+            {:ok, parse_tracks(body["result"], [])}
+          :result ->
+            {:ok, body["result"]}
+          :success ->
+            {:ok, :success}
+          _ ->
+            {:ok, body["value"]}
+        end
+      error ->
+        error_response(error)
+    end
+  end
+
+  def error_response(%{"error" => %{"data" => %{"message" => message}}}) do
+    {:error, message}
+  end
+  def error_response(%{"error" => %{"message" => message}}) do
+    {:error, message}
+  end
+  def error_response({:error, message}), do: {:error, message}
+  def error_response(message) when is_binary(message) do
+    {:error, message}
+  end
+  def error_response(_) do
+    {:error, "invalid response"}
+  end
+
+  # TlTrack parsing
+  def parse_tl_tracks(nil, _accumulator), do: nil
+  def parse_tl_tracks([tl_track | tl_tracks], accumulator) when is_list(accumulator) do
+    parse_tl_tracks(tl_tracks, [parse_tl_track(tl_track)] ++ accumulator)
+  end
+  def parse_tl_tracks(%{"__model__" => "TlTrack"} = tl_track_data, _accumulator) do
+    parse_tl_track(tl_track_data)
+  end
+  def parse_tl_tracks([], accumulator), do: accumulator
+
+  def parse_tl_track(%{"__model__" => "TlTrack"} = tl_track_data) do
+    %Mopidy.TlTrack{
+      tlid: tl_track_data["tlid"],
+      track: parse_track(tl_track_data["track"])
+    }
+  end
+  def parse_tl_track(_), do: nil
+
+  # Track parsing
+  def parse_tracks(nil, _accumulator), do: nil
+  def parse_tracks([track | tracks], accumulator) when is_list(accumulator) do
+    parse_tracks(tracks, [parse_track(track)] ++ accumulator)
+  end
+  def parse_tracks(%{"__model__" => "Track"} = track_data, _accumulator) do
+    parse_track(track_data)
+  end
+  def parse_tracks([], accumulator), do: accumulator
+
+  def parse_track(%{"__model__" => "Track"} = track_data) do
+    %Mopidy.Track{
+      name: track_data["name"],
+      uri: track_data["uri"],
+      album: parse_album(track_data["album"]),
+      artists: parse_artists(track_data["artists"], [])
+    }
+  end
+  def parse_track(_), do: nil
+
+  # Artist parsing
+  def parse_artists([artist | artists], accumulator) when is_list(accumulator) do
+    parse_artists(artists, [parse_artist(artist)] ++ accumulator)
+  end
+  def parse_artists([], accumulator), do: accumulator
+
+  def parse_artist(%{"__model__" => "Artist"} = artist_data) do
+    %Mopidy.Artist{
+      name: artist_data["name"],
+      uri: artist_data["uri"]
+    }
+  end
+
+  # Album parsing
+  def parse_album(%{"__model__" => "Album"} = album_data) do
+    %Mopidy.Album{
+      name: album_data["name"],
+      uri: album_data["uri"]
+    }
   end
 
   @doc """
