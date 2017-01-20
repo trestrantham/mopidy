2bc5f13dd40a437358c740ac38752be7e94285b9
Chris Bucchere <chris@bucchere.com>
2017-01-20 10:53:58 -0800

    remove dependency on inch

--- mix.exs
+++ mix.exs
@@ -38,7 +38,6 @@ defmodule Mopidy.Mixfile do
       {:ex_doc, "~> 0.11", only: :dev},
       {:excoveralls, "~> 0.4", only: :test},
       {:httpotion, "~> 3.0.0"},
-      {:inch_ex, "~> 0.4", only: :docs},
       {:mix_test_watch, "~> 0.2", only: :test},
       {:poison, "~> 3.0"},
       {:socket, "~> 0.3"}
